508489bae3ae553a5a3ce2f116fe1cf9bef37073
Poorani Karunanithi <Pooranikarunanithi09@gmail.com>
2021-12-05 15:49:27 +0100

    database update

--- database/mydb.sql
+++ database/mydb.sql
@@ -1,10 +1,35 @@
 CREATE DATABASE IF NOT EXISTS getflix;
 USE getflix;
 
-CREATE TABLE Persons (
-	PersonID int,
-	LastName varchar(255),
-	FirstName varchar(255)
-);
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(25) NOT NULL,
+  `user_firstname` varchar(255) NOT NULL,
+  `user_lastname` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_uid` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+
 
 
